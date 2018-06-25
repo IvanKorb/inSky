d39321c06cfbd5289fdca04330874bd832a28ccd
Ivan Kor <kor_ivan@inbox.ru>
2018-06-25 12:49:31 +0600

    Add registration  user

---
+++ src/main/java/com/example/inSky/controller/RegistrationController.java
@@ -0,0 +1,39 @@
+package com.example.inSky.controller;
+
+import com.example.inSky.domain.Role;
+import com.example.inSky.domain.User;
+import com.example.inSky.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping("/registration")
+    public String registration(){
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(User user, Map<String, Object> model){
+        User userFromDb = userRepo.findByUsername(user.getUsername());
+
+        if(userFromDb != null){
+            model.put("message", "User exists");
+            return "registration";
+        }
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepo.save(user);
+
+        return "redirect:/login";
+    }
+}
---
+++ src/main/java/com/example/inSky/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package com.example.inSky.repos;
+
+import com.example.inSky.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
