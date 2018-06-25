eecbae37932d73d98bae20b59a595fc329d87c31
Ivan Kor <kor_ivan@inbox.ru>
2018-06-25 11:39:02 +0600

    add connection db

---
+++ src/main/java/com/example/inSky/repos/MessageRepo.java
@@ -0,0 +1,11 @@
+package com.example.inSky.repos;
+
+import com.example.inSky.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+}
